a714774ca1a74013ff5bad81496a3452e1c166f7
Cluebie van Toshi <Cluebie@users.noreply.huggingface.co>
2025-06-05 16:21:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_current_time_in_timezone(timezone: str) -> str:
 
 
 final_answer = FinalAnswerTool()
-currency_rates = Get_currency_conversion_tool()
+#currency_rates = Get_currency_conversion_tool()
 
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
 # model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
@@ -70,7 +70,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer, currency_rates], ## add your tools here (don't remove final answer)
+    tools=[final_answer, dice_tool], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
